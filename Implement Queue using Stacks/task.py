289c3fabae552dc23a2e6907820017f6bb324722
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-06 16:13:10 +0300

    task completed

--- Implement Queue using Stacks/task.py
+++ Implement Queue using Stacks/task.py
@@ -1,37 +1,123 @@
-class MyQueue(object):
+"""
+Implement Queue using Stacks"""
+
+class Stack:
+    """
+    Data structure stack.
+    """
 
     def __init__(self):
-        pass
+        """
+        Initializes an empty stack.
+        """
+        self.__index = []
 
-    def push(self, x):
+    def __len__(self) -> int:
         """
-        :type x: int
-        :rtype: None
-        """
-        
+        Returns the number of elements in the stack.
 
-    def pop(self):
+        :return: The number of elements in the stack.
         """
-        :rtype: int
+        return len(self.__index)
+
+    def push(self, item):
         """
-        
+        Adds an item to the top of the stack.
+
+        :param item: The item to be added to the stack.
+        """
+        self.__index.append(item)
 
     def peek(self):
         """
-        :rtype: int
-        """
-        
+        Returns the top item of the stack without removing it.
 
-    def empty(self):
+        :return: The top item of the stack.
         """
-        :rtype: bool
+        if len(self) == 0:
+            raise ValueError('Stack is empty')
+        return self.__index[-1]
+
+    def pop(self):
         """
-        
+        Removes and returns the top item of the stack.
+
+        :return: The top item of the stack.
+        """
+        if len(self) == 0:
+            raise ValueError('Stack is empty')
+        return self.__index.pop()
+
+    def __str__(self)-> str:
+        """
+        Returns a string representation of the stack.
+
+        :return: A string representing the stack.
+        """
+        return str(self.__index)
 
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+class MyQueue:
+    """
+    A queue implemented using two stacks.
+    """
+
+    def __init__(self):
+        """
+        Initializes an empty queue using two stacks.
+        """
+        self.stack_one = Stack()
+        self.stack_final = Stack()
+
+    def push(self, x):
+        """
+        Adds an element to the end of the queue.
+
+        :param x: The element to be added to the queue.
+        :type x: Any
+        """
+        self.stack_one.push(x)
+
+    def pop(self):
+        """
+        Removes and returns the element at the front of the queue.
+
+        :return: The element at the front of the queue.
+        """
+        if not self.stack_final:
+            while self.stack_one:
+                last_el = self.stack_one.pop()
+                self.stack_final.push(last_el)
+
+        return self.stack_final.pop()
+
+    def peek(self):
+        """
+        Returns the element at the front of the queue without removing it.
+
+        :return: The element at the front of the queue.
+        """
+        if not self.stack_final:
+            while self.stack_one:
+                last_el = self.stack_one.pop()
+                self.stack_final.push(last_el)
+
+        return self.stack_final.peek()
+
+    def empty(self) -> bool:
+        """
+        Checks if the queue is empty.
+
+        :return: True if the queue is empty, False otherwise.
+        """
+        return len(self.stack_one) == 0 and len(self.stack_final) == 0
+
+ # test from leetcode
+
+# my_queue = MyQueue()
+# my_queue.push(1)
+# my_queue.push(2)
+# print(my_queue.peek())
+# my_queue.pop()
+# print(my_queue.stack_final)
+# print(my_queue.empty())
